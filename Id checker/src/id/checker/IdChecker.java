8360a11018833e9d822087ed6503903ba3d60cd6
Mishan Madhupa <mishancool2016@gmail.com>
2020-03-12 22:18:02 +0530

    Start id checker app

---
+++ Id checker/src/id/checker/IdChecker.java
@@ -0,0 +1,15 @@
+package id.checker;
+
+/**
+ *
+ * @author MISHAN
+ */
+public class IdChecker {
+
+    
+    public static void main(String[] args) {
+        Id a = new Id();
+        a.setVisible(true);
+    }
+    
+}
